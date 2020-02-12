70c06820fb9be3e9ea004387fd8f4b7b54d9c19f
rajesh.ranjan-at-763089524799 <N1jTeVlYNmFzQikq2/4j/5OFyNNecEwo22s70yr071Q=>
2020-02-12 01:04:49 -0800

    added user detail

--- resolver-app/resolvers.js
+++ resolver-app/resolvers.js
@@ -23,9 +23,13 @@ const Mutation = {
   login: async (root, args, context, info) => {
     try {
       let redata = await trigger.meanHttpCall("post","login", args);
-      var token=redata.headers['set-cookie']
-      redata.headers.setcookie = token;
-      return redata;
+      if(redata.data && redata.data.success==false){
+        return redata;
+      }else{
+        var token=redata.headers['set-cookie']
+        redata.headers.setcookie = token;
+        return redata;
+      }
     } catch (error) {
       logger.error(error);
         throw new Error(error.response.data)
@@ -43,14 +47,21 @@ const Mutation = {
   },
   signin: async (root, args, context, info) => {
     try {
-      console.log(args)
+    
       let redata = await trigger.meanHttpCall(
         "post",
         "registration",
         args
       );
-      logger.info(redata.data);
-      return redata;
+      if(redata.data.success==false){
+        logger.info(redata.data);
+        redata.data.message='User Account already exist ? Thank You'
+        return redata
+      }else{
+        logger.info(redata.data);
+        return redata;
+      }
+      
     } catch (error) {
       logger.error(error);
       console.log(error.response.data)
