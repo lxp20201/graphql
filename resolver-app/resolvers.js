6be39f9e16a46b7a39c00564b98e6ed8e9a813e2
karthi D <karthi.d@cintanatech.com>
2020-02-19 13:12:06 +0530

    link status verification added

--- resolver-app/resolvers.js
+++ resolver-app/resolvers.js
@@ -154,6 +154,28 @@ const Mutation = {
         throw new Error(error.response.data);
       }
     }
+  },
+  checklinkstatus: async (root, args, context, info) => {
+    try {
+      let redata = await trigger.meanHttpCall(
+        "post",
+        "/checklinkstatus",  
+        args
+      );
+      if (redata.data && redata.data.success == false) {       
+        return redata.data;
+      } else {
+        return redata.data;
+      }
+    } catch (error) {
+      logger.error(error);
+      if (error.response.data && error.response.data.error) {
+        error.response.data.message = error.response.data.error;
+        return error.response;
+      } else {
+        throw new Error(error.response.data);
+      }
+    }
   }
 };
 
